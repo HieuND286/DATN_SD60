69f63bac0a19b494be20e53d8d65e3dab105c039
Thanh Tuyen <tuyenbt1011@gmail.com>
2024-06-04 22:53:32 +0700

    no message

--- src/main/java/com/example/backend/model/AdminDanhMucRespon.java
+++ src/main/java/com/example/backend/model/AdminDanhMucRespon.java
@@ -1,2 +1,14 @@
-package com.example.backend.model;public interface AdminDanhMucRespon {
+package com.example.backend.model;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public interface AdminDanhMucRespon {
+    @Value("#{target.ma}")
+    String getMa();
+
+    @Value("#{target.ten}")
+    String getTen();
+
+    @Value("#{target.ma}")
+    String getTrangThai();
 }
