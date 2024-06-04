9b31664323c2cf08b7f0ad7f7b66743e3ba904fb
cuongvd203 <140222491+cuongvd203@users.noreply.github.com>
2024-06-04 14:41:59 +0700

    cuong

---
+++ src/main/java/com/example/demo/entity/SanPham.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "san_pham")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+
+public class SanPham {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String ma;
+    private String ten;
+    private LocalDateTime ngayTao;
+    private LocalDateTime ngaySua;
+    private String nguoiTao;
+    private String nguoiSua;
+    private int trangThai;
+
+}
